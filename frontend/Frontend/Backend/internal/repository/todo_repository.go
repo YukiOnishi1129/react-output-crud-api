048806f26d4c3abc15ad28e4f210aacef118b589
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-16 01:26:22 +0900

    fix

--- frontend/Frontend/Backend/internal/repository/todo_repository.go
+++
@@ -1,7 +0,0 @@
-type TodoRepository interface {
-	FindAll(ctx context.Context) ([]*domain.Todo, error)
-	FindByID(ctx context.Context, input dto.FindByIDInput) (*domain.Todo, error)
-	Create(ctx context.Context, todo *domain.Todo) error
-	Update(ctx context.Context, todo *domain.Todo) error
-	Delete(ctx context.Context, id uuid.UUID) error
-} 
