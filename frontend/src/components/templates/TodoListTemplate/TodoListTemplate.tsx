bccfad1bfed25e34e59ea118716326ce354781ea
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-16 03:06:20 +0900

    useTodoListTemplate

--- frontend/src/components/templates/TodoListTemplate/TodoListTemplate.tsx
+++ frontend/src/components/templates/TodoListTemplate/TodoListTemplate.tsx
@@ -1,57 +1,13 @@
-import { useMemo, useState, useEffect, useCallback } from "react";
-import { useForm, Controller } from "react-hook-form";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller } from "react-hook-form";
 import { InputFormSection } from "../../molecules";
 import { BaseLayout, TodoList } from "../../organisms";
 
-import { getTodos, deleteTodo } from "../../../apis/todo";
-import { TodoType } from "../../../types/Todo";
+import { useTodoListTemplate } from "./useTodoListTemplate";
 
 import styles from "./style.module.css";
 
-const schema = z.object({
-  keyword: z.string(),
-});
-
 export const TodoListTemplate = () => {
-  const [todoList, setTodoList] = useState<Array<TodoType>>([]);
-
-  const { control, watch } = useForm({
-    resolver: zodResolver(schema),
-    defaultValues: { keyword: "" },
-  });
-  const searchKeyword = watch("keyword");
-
-  /* 表示用TodoList */
-  const showTodoList = useMemo(() => {
-    const regexp = new RegExp("^" + searchKeyword, "i");
-    return todoList.filter((todo) => {
-      // 検索キーワードに部分一致したTodoだけを一覧表示する
-      return todo.title.match(regexp);
-    });
-    // useMemoの第二引数([originTodoList, searchKeyword])に依存して処理が実行される
-    // originTodoListとsearchKeywordの値が変更される度にfilterの検索処理が実行
-    // ただし結果が前回と同じならキャッシュを返却し処理は実行されない(無駄な処理を省いている)
-    // 詳しくはuseMemoを調べてください。
-  }, [todoList, searchKeyword]);
-
-  const fetchTodoList = useCallback(async () => {
-    const response = await getTodos();
-    if (!response) return;
-
-    setTodoList(response.todos);
-  }, []);
-
-  const handleDeleteTodo = useCallback((id: string, title: string) => {
-    if (window.confirm(`「${title}」のtodoを削除しますか？`)) {
-      deleteTodo({ id });
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchTodoList();
-  }, [fetchTodoList]);
+  const { control, showTodoList, handleDeleteTodo } = useTodoListTemplate();
 
   return (
     <BaseLayout title={"TodoList"}>
